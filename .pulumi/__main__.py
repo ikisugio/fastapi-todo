b3c0675679969db09d90c60810deaae3344e954b
ikisugio <drymtn721@gmail.com>
2023-08-02 17:25:02 +0900

    1111

---
+++ .pulumi/__main__.py
@@ -0,0 +1,52 @@
+import pulumi
+from pulumi_aws import ec2
+from dotenv import load_dotenv
+import os  # Add this line
+
+# AWSの環境変数を読み込む
+load_dotenv('../.env/.env.dev')
+aws_envs = {key.lower(): val for key, val in os.environ.items()}  # Use os module here
+
+# DockerとFastAPIアプリケーションを起動するためのスクリプト
+user_data = f"""
+#!/bin/bash
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+docker run -d -p 80:80 {aws_envs['dockerhub_username']}/fastapi-todo  # Use aws_envs instead of envs
+"""
+
+# セキュリティグループのイングレスルール設定
+ingress_args = {
+    'protocol': 'tcp',
+    'from_port': 80,
+    'to_port': 80,
+    'cidr_blocks': ['0.0.0.0/0'],
+}
+
+# セキュリティグループの設定
+security_group_config = {
+    'name': 'my_security_group',
+    'description': 'Enable HTTP access',
+    'ingress': [
+        ec2.SecurityGroupIngressArgs(**ingress_args),
+    ],
+}
+
+# セキュリティグループの作成
+security_group = ec2.SecurityGroup(**security_group_config)
+
+# EC2インスタンスの作成
+instance = ec2.Instance(
+    instance_type=aws_envs['instance_type'],  # Use aws_envs instead of envs
+    ami=aws_envs['ubuntu_ami_id'],  # Use aws_envs instead of envs
+    vpc_security_group_ids=[security_group.id], # セキュリティグループのIDを指定
+    user_data=user_data, # DockerとFastAPIアプリケーションを起動するスクリプト
+    tags={
+        "Name": aws_envs['instance_tag_name'],  # Use aws_envs instead of envs
+    },
+    **aws_envs # アンパックして環境変数を渡します
+)
+
+pulumi.export('instance_id', instance.id)
+pulumi.export('public_ip', instance.public_ip)
+pulumi.export('public_dns', instance.public_dns)
---
+++ app/routers.py
@@ -0,0 +1,53 @@
+from typing import List
+
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/todos/", response_model=schemas.Todo)
+def create_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+    db_todo = crud.get_todo_by_title(db, title=todo.title)
+    if db_todo:
+        raise HTTPException(status_code=400, detail="Title already registered")
+    return crud.create_user(db=db, todo=todo)
+
+@app.get("/todos/", response_model=List[schemas.Todo])
+def read_todos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    todos = crud.get_todos(db, skip=skip, limit=limit)
+    return todos
+
+@app.get("/todos/{todo_id}", response_model=schemas.Todo)
+def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = crud.get_todo(db, todo_id=todo_id)
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return db_todo
+
+@app.put("/todos/{todo_id}", response_model=schemas.Todo)
+def update_todo(todo_id: int, todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+    db_todo = crud.get_todo(db, todo_id=todo_id)
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return crud.update_user(db=db, todo=todo)
+
+@app.delete("/todos/{todo_id}", response_model=schemas.Todo)
+def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = crud.get_todo(db, todo_id=todo_id)
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    crud.delete_todo(db=db, todo=db_todo)
+    return db_todo
